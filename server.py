b69a0a3e9da9ab28b846b5bd53149c0348ef9d0a
James Payor <james@payor.com.au>
2016-06-26 11:54:47 -0700

    Add history page.

--- server.py
+++ server.py
@@ -3,14 +3,19 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.template_filter('orderBets')
+def order_bets(bets):
+    return sorted(bets, key = lambda (name, (t,f)): (t + f, name != 'total'))
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/bets")
-def bets():
-	
+@app.route("/history")
+def history():
+	from state import State
+	return render_template('history.html', state=State.load())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
