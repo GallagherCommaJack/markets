ef0dda6e89d6986ea5e751c9de78882065ead287
Harmanas Chopra <harmchop@Harmanass-MacBook-Air.local>
2016-06-26 13:39:50 -0700

    Updated mean.

--- players/harmanas.py
+++ players/harmanas.py
@@ -1,22 +1,24 @@
 def bet(bets, wealth, round):
-    # We can get the bets dummy has placed on true and false by querying the bets table
-    total_true, total_false = bets['total']
-    players = len(wealth)
-    ps = total_true / (total_true + total_false)
-    total = (total_true + total_false) / players
+  total_true, total_false = bets['total']
+  players = len(wealth)
+  ps[0] = float(total_true / (total_true + total_false))
+  total = float((total_true + total_false)) / players
 
-    # We can also see how much wealth a player has by querying wealth
-    w_list = list(d.items())
-    w_sorted = w_list.sort(key= lambda tup: tup[1])
-	
-    # This is wrong, it's supposed to take an average.
-    for w in w_sorted[0:3]:
-	t, f = bets[w[0]]
-	p = (p + (t / (t+f)))/2
-	
-    if round > 0:
-	return (p * total, (1-p) * total)
-    else:
-	return 0, 0
-		
+  # We can also see how much wealth a player has by querying wealth
+  w_list = list(d.items())
+  w_sorted = w_list.sort(key = lambda tup: tup[1])
 
+  # This is wrong, it 's supposed to take an average.
+  for i, w in enumerate(w_sorted[0: 3]):
+      t, f = bets[w[0]]
+  ps[i + 1] = t / (t + f)
+
+  p = mean(ps)
+
+  if round > 0:
+      return (p * total, (1 - p) * total)
+  else :
+      return 0.0, 0.0
+
+def mean(ps):
+  return sum(ps) / len(ps)
