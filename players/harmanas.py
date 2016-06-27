38f8915b4c3031ab546f006a5e171ccdc386ad65
James Payor <james@payor.com.au>
2016-06-26 22:45:33 -0700

    Fix another bug in Harmanas' player.

--- players/harmanas.py
+++ players/harmanas.py
@@ -5,7 +5,7 @@ def bet(bets, wealth, round):
   total = float((total_true + total_false)) / players
 
   # We can also see how much wealth a player has by querying wealth
-  w_list = list(d.items())
+  w_list = list(wealth.items())
   w_sorted = w_list.sort(key = lambda tup: tup[1])
 
   # This is wrong, it 's supposed to take an average.
