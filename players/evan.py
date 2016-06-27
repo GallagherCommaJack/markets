d90836f06278b72d0aca0e73fa3a81f1875543a2
monikerepsilon <shortytwometers@gmail.com>
2016-06-26 20:12:01 -0700

    estimate 1 brazilian

--- players/evan.py
+++ players/evan.py
@@ -1,3 +1,5 @@
+import math
+
 def bet(bets,wealth,round):
     # We can get the bets dummy has placed on true and false by querying the bets table
     dummy_true, dummy_false = bets['dummy']
@@ -10,7 +12,7 @@ def bet(bets,wealth,round):
 
     # less dummy and total
     nPlayers = len(bets) - 2
-    nTrue, nFalse = 0,0
+    nTrue, nFalse = 1,1
     for player in bets:
         if player == 'dummy' or player == 'total':
             continue
@@ -19,26 +21,64 @@ def bet(bets,wealth,round):
         else:
             nFalse += 1
 
-##    totalWealth = 0.0
-##    for player in wealth:
-##        if player == 'dummy' or player == 'total':
-##            continue
-##        totalWealth += wealth[player]
-##
-##    weightedBets = {}
-##
-##    for player in bets:
-##        if player == 'dummy' or player == 'total':
-##            continue
-##        weightedBets[player] = (bets[player][0] * wealth[player] / totalWealth, bets[player][1] * wealth[player] / totalWealth)
+    totalWealth = 0.0
+    for player in wealth:
+        if player == 'dummy' or player == 'total':
+            continue
+        totalWealth += wealth[player]
+
+    weightedBets = {}
+    playerProb = {}
+    reasonableBets = []
+
+    # Round 0: log_10 (Population of Brazil) / 10 > rand[0,1]
+    myBestGuess = math.log10(5.0 * 10.0**8) / 10
+    crazyLow = math.log10(1.0 * 10.0**7) / 10
+    crazyHigh = math.log10(1.0 * 10.0**9) / 10
+
+    riskiness = dict([(p, '0.5') for p in bets])
+
+    for player in bets:
+        if player == 'dummy' or player == 'total' or wealth[player] == 0.0 or sum(bets[player]) == 0.0:
+            continue
+        weightedBets[player] = (bets[player][0] * wealth[player] / totalWealth, bets[player][1] * wealth[player] / totalWealth)
+
+        if bets[player][0] > 0.0 and bets[player][1] > 0.0:
+            playerProb[player] = (bets[player][0] / (bets[player][0] + bets[player][1]))
+            if playerProb[player] >= crazyLow and playerProb[player] <= crazyHigh:
+                reasonableBets.append(playerProb[player])
+
+        # heuristic--are they too confident (somehow) relative to their wealth? (whatever that means)
+        # actually this needs to depend on the market prob more directly
+        else:
+            # market is too low, are they strong on no?
+            if marketProb < myBestGuess:
+                playerProb[player] = 1 - bets[player][1] / wealth[player]
+            else: # market too high, are they strong on yes?
+                playerProb[player] = bets[player][0] / wealth[player]
+
+            if playerProb[player] >= crazyLow and playerProb[player] <= crazyHigh:
+                reasonableBets.append(playerProb[player])
 
     playerRatio = float(nTrue) / float(nFalse)
 
-    if 0 == round:
-        # Round 0: "Define p to be marketProb. Question: is 'marketProb > 0.7'"
-        if playerRatio >= 0.7:
-            return myBet, 0.0
-        else:
-            return 0.0, myBet
-    else:
-        return 0.0, 0.0
+    reasonableMarketGuess = myBestGuess
+    if len(reasonableBets) > 0:
+        reasonableMarketGuess = sum(reasonableBets) / len(reasonableBets)
+    myAdjustedProb = (myBestGuess + reasonableMarketGuess) / 2.0
+
+    # if more time: throw out crazy bets
+    myAdjustedProb = myBestGuess
+
+    if marketProb < myAdjustedProb:
+        return myBet, 0.0
+    return 0.0, myBet
+
+##    if 0 == round:
+##        # Round -1: "Define p to be marketProb. Question: is 'marketProb > 0.7'"
+##        if playerRatio >= 0.7:
+##            return myBet, 0.0
+##        else:
+##            return 0.0, myBet
+##    else:
+##        return 0.0, 0.0
