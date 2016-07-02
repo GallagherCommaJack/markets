4f38f34b69cdc04cff4d85ef4f35d0808e1fccdc
MSFP Market <james@payor.com.au>
2016-07-02 07:53:03 +0000

    Round 3 players (just Evan...)

--- players/evan.py
+++ players/evan.py
@@ -4,17 +4,44 @@ def bet(bets,wealth,round):
     # We can get the bets dummy has placed on true and false by querying the bets table
     dummy_true, dummy_false = bets['dummy']
     total_true, total_false = bets['total']
-
-    # today's DOW
-    # 17,409.72
-    # so very likely number like
-    #   xxx-x(3-6)71
-
     marketProb = total_true / (total_true + total_false)
-    myMoney = wealth['evan']
+    myWealth = wealth['evan']
+##
+##    # today's DOW
+##    # 17,409.72
+##    # so very likely number like
+##    #   xxx-x(3-6)71
+##
+##
+##
+##    myEstProb = 0.20
+##
+##    if marketProb > myEstProb:
+##        return 0.0, min(myMoney * 0.5, (marketProb - myEstProb) * myMoney)
+##    return 0.0, 0.0
+##
+##    myBet = myMoney * 0.5
+##
+##    # less dummy and total
+    nPlayers = len(bets) - 2
+    nTrue, nFalse = 1,1
+    for player in bets:
+        if player == 'dummy' or player == 'total':
+            continue
+        if bets[player][0] > bets[player][1]:
+            nTrue += 1
+        else:
+            nFalse += 1
 
-    myEstProb = 0.20
+    if marketProb > 0.8:
+        return 0.00, myWealth * (marketProb - 0.8) * (0.5 / 0.2)
+    if marketProb < 0.2:
+        return myWealth * (0.2 - marketProb) * (0.5 / 0.2), 0.00
 
-    if marketProb > myEstProb:
-        return 0.0, min(myMoney * 0.5, (marketProb - myEstProb) * myMoney)
+    if marketProb > 0.4 and marketProb < 0.6:
+        if nTrue > nFalse:
+            return myWealth * marketProb, 0.00
+        else:
+            return 0.00, myWealth * marketProb
+
     return 0.0, 0.0
